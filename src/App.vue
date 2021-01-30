e06bcef716186b9f0466cb5ebfc496c0f813dfc4
Victor Allen <vamuigua@gmail.com>
2021-01-31 01:34:57 +0300

    update: Added more details from api and styling + .env file Added changing of temperature units between degrees celsius and fahrenheit when the temp is clicked App now displays an icon of the current weather condition Moved the API to the .env file

--- src/App.vue
+++ src/App.vue
@@ -6,14 +6,31 @@
     "
   >
     <main>
+      <div class="app-title">
+        <h1>Vue Weather ⛅</h1>
+        <small
+          ><a href="http://github.com/vamuigua" target="_blank"
+            >By Victor Allen</a
+          ></small
+        >
+      </div>
+
       <div class="search-box">
         <input
           type="text"
           class="search-bar"
-          placeholder="Search..."
+          placeholder="Enter city of interest..."
           v-model="query"
           @keyup.enter="fetchWeather"
         />
+
+        <div class="search-button">
+          <button @click="fetchWeather" :disabled="isSearching">Search</button>
+        </div>
+      </div>
+
+      <div class="errors" v-if="weather.cod != '200'">
+        <p>{{ weather.message }}</p>
       </div>
 
       <div class="weather-wrap" v-if="typeof weather.main != 'undefined'">
@@ -25,8 +42,17 @@
         </div>
 
         <div class="weather-box">
-          <div class="temp">{{ Math.round(weather.main.temp) }}°c</div>
-          <div class="weather">{{ weather.weather[0].description }}</div>
+          <div class="temp" @click="changeTempUnits">
+            {{ temp }}
+          </div>
+          <div class="weather-details">
+            <div class="weather-img">
+              <img :src="weather_icon" alt="weather_img" />
+            </div>
+            <div class="weather-description">
+              {{ weather.weather[0].description }}
+            </div>
+          </div>
         </div>
       </div>
     </main>
@@ -38,14 +64,19 @@ export default {
   name: "App",
   data() {
     return {
-      api_key: "71f034e46c384337106e628121369a51",
+      api_key: process.env.VUE_APP_API_KEY,
       url_base: "https://api.openweathermap.org/data/2.5/",
       query: "",
       weather: {},
+      isSearching: false,
+      weather_icon: "",
+      temp: "",
+      isMetric: true,
     };
   },
   methods: {
     fetchWeather() {
+      this.isSearching = true;
       fetch(
         `${this.url_base}weather?q=${this.query}&units=metric&appid=${this.api_key}`
       )
@@ -53,9 +84,13 @@ export default {
           return res.json();
         })
         .then(this.setResuts);
+      this.query = "";
     },
     setResuts(results) {
       this.weather = results;
+      this.temp = Math.round(this.weather.main.temp) + "°c";
+      this.weather_icon = `http://openweathermap.org/img/wn/${this.weather.weather[0].icon}@2x.png`;
+      this.isSearching = false;
     },
     dateBuilder() {
       let d = new Date();
@@ -87,6 +122,15 @@ export default {
       let month = months[d.getMonth()];
       let year = d.getFullYear();
       return `${day} ${date} ${month} ${year}`;
+    },
+    changeTempUnits() {
+      this.isMetric = !this.isMetric;
+      if (this.isMetric) {
+        this.temp = Math.round(this.weather.main.temp) + "°c";
+      } else {
+        const fahrenheit = Math.round((this.weather.main.temp * 9) / 5 + 32);
+        this.temp = fahrenheit + "°F";
+      }
     },
   },
 };
@@ -124,6 +168,31 @@ main {
   );
 }
 
+.app-title {
+  margin: 5px auto;
+  color: #fff;
+  font-size: 25px;
+  font-weight: 500;
+  text-align: center;
+  text-shadow: 2px 3px rgba(0, 0, 0, 0.5);
+}
+
+.app-title small a {
+  text-decoration: none;
+  color: #fff;
+  transition: 0.4s;
+}
+
+.app-title small a:hover {
+  padding: 5px;
+  margin: 10px auto;
+  border-radius: 5px;
+  color: black;
+  text-shadow: none;
+  box-shadow: 0px 0px 8px rgba(0, 0, 0, 0.25);
+  background-color: rgba(255, 255, 255, 0.5);
+}
+
 .search-box {
   width: 100%;
   margin-bottom: 30px;
@@ -144,11 +213,47 @@ main {
   border-radius: 0px 16px 0px 16px;
   transition: 0.4s;
 }
+.search-box .search-bar::placeholder {
+  color: #2b2a2a;
+}
 
 .search-box .search-bar:focus {
   box-shadow: 0px 0px 16px rgba(0, 0, 0, 0.25);
   background-color: rgba(255, 255, 255, 0.75);
   border-radius: 16px 0px 16px 0px;
+}
+
+.search-box .search-button {
+  margin: 5px auto;
+  text-align: center;
+  padding: 10px 20px;
+  display: block;
+}
+
+.search-box button {
+  font-size: 1.5em;
+  text-shadow: 1px 1px 2px rgba(0, 0, 0, 0.5);
+  color: white;
+  padding: 5px;
+  border-radius: 10px;
+  appearance: none;
+  border: none;
+  outline: none;
+  width: 50%;
+  box-shadow: 4px 4px rgba(0, 0, 0, 0.25);
+  background-color: rgba(255, 255, 255, 0.2);
+  border-radius: 10px;
+  cursor: pointer;
+  transition: 0.4s;
+}
+
+.search-box button:hover {
+  background-color: rgb(255, 255, 255);
+  color: black;
+}
+.search-box button:disabled {
+  background-color: rgba(255, 255, 255, 0.1);
+  cursor: not-allowed;
 }
 
 .location-box .location {
@@ -180,11 +285,31 @@ main {
   margin: 30px 0px;
   box-shadow: 3px 6px rgba(0, 0, 0, 0.25);
 }
-.weather-box .weather {
+.weather-box .temp:hover {
+  box-shadow: 0px 0px 16px rgba(0, 0, 0, 0.25);
+  background-color: rgba(255, 255, 255, 0.75);
+  cursor: pointer;
+}
+
+.weather-box .weather-details {
+  display: inline-block;
+  box-sizing: border-box;
+  margin-left: 20px;
+}
+
+.weather-box .weather-description {
   color: #fff;
   font-size: 48px;
   font-weight: 700;
   font-style: italic;
   text-shadow: 3px 6px rgba(0, 0, 0, 0.25);
+}
+
+.errors p {
+  color: #fff;
+  font-size: 35px;
+  font-weight: 500;
+  text-align: center;
+  text-shadow: 1px 3px rgba(0, 0, 0, 0.25);
 }
 </style>
